c61b48721c91cba892e41b8eb2f167994cb0ec0f
korzenim <mike.korzen@outlook.com>
2024-04-14 17:26:00 -0400

    sub

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE departments (
+	dept_no VARCHAR(255) PRIMARY KEY,
+	dept_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE titles (
+	title_id VARCHAR(255) PRIMARY KEY,
+	title VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(255) NOT NULL,
+	birth_date VARCHAR(255) NOT NULL,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	sex VARCHAR(255) NOT NULL,
+	hire_Date VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(255) NOT NULL,
+    emp_no INT NOT NULL,
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(255) NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE salaries (
+    emp_no BIGINT NOT NULL,
+    salary BIGINT NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
